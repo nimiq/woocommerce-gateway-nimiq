26bff8376e7388a22930b11fc37a247c81c5ceeb
sisou <hello@soerenschwert.de>
2018-06-05 19:14:55 +0200

    Initial commit, WIP

---
+++ README.md
@@ -0,0 +1,48 @@
+=== WooCommerce Nimiq Gateway ===
+
+ - Contributors: skyverge, beka.rice, nimiq
+ - Tags: woocommerce, payment gateway, gateway, nimiq, cryptocurrency
+ - Requires at least: 3.8
+ - Tested up to: 4.3
+ - Requires WooCommerce at least: 3.0
+ - Tested WooCommerce up to: 3.4
+ - Stable Tag: 1.1.0
+ - License: GPLv3
+ - License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+== Description ==
+
+> **Requires: WooCommerce 3.0+**
+
+...
+
+When an order is submitted via the Nimiq payment method, the order will be placed "on-hold".
+
+= More Details =
+ - See the [product page](http://www.skyverge.com/product/woocommerce-offline-gateway/) for full details and documentation
+
+== Installation ==
+
+1. Be sure you're running WooCommerce 3.0+ in your shop.
+2. You can: (1) upload the entire `woocommerce-gateway-nimiq` folder to the `/wp-content/plugins/` directory, (2) upload the .zip file with the plugin under **Plugins &gt; Add New &gt; Upload**
+3. Activate the plugin through the **Plugins** menu in WordPress
+4. Go to **WooCommerce &gt; Settings &gt; Checkout** and select "Nimiq" to configure
+
+== Frequently Asked Questions ==
+
+**What is the text domain for translations?**
+The text domain is `wc-gateway-nimiq`.
+
+**Can I fork this?**
+Please do! This is meant to be a simple starter Nimiq gateway, and can be modified easily.
+
+== Changelog ==
+
+= 2018.06.05 - version 1.1.0 =
+ * adapted for payments with Nimiq
+
+= 2015.07.27 - version 1.0.1 =
+ * Misc: WooCommerce 2.4 Compatibility
+
+= 2015.05.04 - version 1.0.0 =
+ * Initial Release
---
+++ woocommerce-gateway-offline.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Nimiq Gateway
+ * Plugin URI:
+ * Description: Pay with Nimiq via the Nimiq Keyguard
+ * Author: Nimiq
+ * Author URI: http://www.nimiq.com/
+ * Version: 1.1.0
+ * Text Domain: wc-gateway-nimiq
+ * Domain Path: /i18n/languages/
+ *
+ * Copyright: (c) 2015-2016 SkyVerge, Inc. (info@skyverge.com) and WooCommerce, Nimiq Network Ltd.
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   WC-Gateway-Nimiq
+ * @author    Nimiq
+ * @category  Admin
+ * @copyright Copyright (c) 2015-2016, SkyVerge, Inc. and WooCommerce, Nimiq Network Ltd.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ *
+ * This Nimiq gateway forks the WooCommerce core "Cheque" payment gateway to create another payment method.
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+
+// Make sure WooCommerce is active
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	return;
+}
+
+
+/**
+ * Add the gateway to WC Available Gateways
+ *
+ * @since 1.0.0
+ * @param array $gateways all available WC gateways
+ * @return array $gateways all WC gateways + Nimiq gateway
+ */
+function wc_nimiq_add_to_gateways( $gateways ) {
+	$gateways[] = 'WC_Gateway_Nimiq';
+	return $gateways;
+}
+add_filter( 'woocommerce_payment_gateways', 'wc_nimiq_add_to_gateways' );
+
+
+/**
+ * Adds plugin page links
+ *
+ * @since 1.0.0
+ * @param array $links all plugin links
+ * @return array $links all plugin links + our custom links (i.e., "Settings")
+ */
+function wc_nimiq_gateway_plugin_links( $links ) {
+
+	$plugin_links = array(
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=nimiq_gateway' ) . '">' . __( 'Configure', 'wc-gateway-nimiq' ) . '</a>'
+	);
+
+	return array_merge( $plugin_links, $links );
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wc_nimiq_gateway_plugin_links' );
+
+
+/**
+ * Nimiq Payment Gateway
+ *
+ * Provides a Nimiq Payment Gateway; mainly for testing purposes.
+ * We load it later to ensure WC is loaded first since we're extending it.
+ *
+ * @class 		WC_Gateway_Nimiq
+ * @extends		WC_Payment_Gateway
+ * @version		1.0.0
+ * @package		WooCommerce/Classes/Payment
+ * @author 		Nimiq
+ */
+add_action( 'plugins_loaded', 'wc_nimiq_gateway_init', 11 );
+
+function wc_nimiq_gateway_init() {
+
+	class WC_Gateway_Nimiq extends WC_Payment_Gateway {
+
+		/**
+		 * Constructor for the gateway.
+		 */
+		public function __construct() {
+
+			$this->id                 = 'nimiq_gateway';
+			$this->icon               = apply_filters('woocommerce_nimiq_icon', '');
+			$this->has_fields         = true;
+			$this->method_title       = __( 'Nimiq', 'wc-gateway-nimiq' );
+			$this->method_description = __( 'Allows Nimiq payments. Orders are marked as "on-hold" when received.', 'wc-gateway-nimiq' );
+
+			// Load the settings.
+			$this->init_form_fields();
+			$this->init_settings();
+
+			// Define user set variables
+			$this->title        = $this->get_option( 'title' );
+			$this->description  = $this->get_option( 'description' );
+			$this->instructions = $this->get_option( 'instructions' );
+
+			// Actions
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+			add_action( 'woocommerce_thankyou_' . $this->id, array( $this, 'thankyou_page' ) );
+			add_action( 'admin_notices', array( $this, 'do_store_nim_address_check' ) );
+
+			// Customer Emails
+			add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
+		}
+
+
+		/**
+		 * Initialize Gateway Settings Form Fields
+		 */
+		public function init_form_fields() {
+
+			$this->form_fields = apply_filters( 'wc_nimiq_form_fields', array(
+
+				'enabled' => array(
+					'title'   => __( 'Enable/Disable', 'wc-gateway-nimiq' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable Nimiq payments', 'wc-gateway-nimiq' ),
+					'default' => 'yes'
+				),
+
+				'nimiq_address' => array(
+					'title'       => __( 'Store NIM Address', 'wc-gateway-nimiq' ),
+					'type'        => 'text',
+					'description' => __( 'Your Nimiq address where customers will send their transactions to.', 'wc-gateway-nimiq' ),
+					'default'     => '',
+					'placeholder' => 'NQ...',
+					'desc_tip'    => true,
+				),
+
+				'title' => array(
+					'title'       => __( 'Payment Title', 'wc-gateway-nimiq' ),
+					'type'        => 'text',
+					'description' => __( 'This controls the title for the payment method the customer sees during checkout.', 'wc-gateway-nimiq' ),
+					'default'     => __( 'Pay with Nimiq', 'wc-gateway-nimiq' ),
+					'desc_tip'    => true,
+				),
+
+				'description' => array(
+					'title'       => __( 'Description', 'wc-gateway-nimiq' ),
+					'type'        => 'textarea',
+					'description' => __( 'Payment method description that the customer will see during checkout.', 'wc-gateway-nimiq' ),
+					'default'     => __( 'Pay for your swag with NIM via the Nimiq Keyguard.', 'wc-gateway-nimiq' ),
+					'desc_tip'    => true,
+				),
+
+				'instructions' => array(
+					'title'       => __( 'Instructions', 'wc-gateway-nimiq' ),
+					'type'        => 'textarea',
+					'description' => __( 'Instructions that will be added to the thank-you page and emails.', 'wc-gateway-nimiq' ),
+					'default'     => __( 'You will receive another email after your payment has been confirmed and we sent your order.' ),
+					'desc_tip'    => true,
+				),
+			) );
+		}
+
+		public function payment_fields() {
+			// $description = $this->get_description();
+			// if ( $description ) {
+			// 	echo wpautop( wptexturize( $description ) );
+			// }
+
+			?>
+				<div class="nim_address_loading">
+					Loading your accounts, please wait...
+				</div>
+				<div class="nim_address_selector">
+					<label>Please select which account you want to pay with:</label><br>
+					<select name="customer_nim_address" id="customer_nim_address">
+						<option>NQ02 EXRU MPQR TSM4 P5PB N6AU 6V71 C7CV D795</option>
+					</select>
+					<input type="hidden" name="transaction_hash">
+				</div>
+				<script src="path/to/keyguard-client.js"></script>
+				<script>
+					var store_nim_address = '<?php echo $this->get_option( 'nimiq_address' ); ?>';
+
+					// Add submit event listener to form, preventDefault()
+
+					// Start interval to fetch block height every 30 minutes
+					// (Make sure it's executed immediately as well)
+
+					// Await keyguard-client connection
+
+					// Get accounts
+
+					// Fill select
+
+					// Hide loading message, unhide select, enable payment button
+
+					// On form submit, generate tx object and call keyguard signSafe/signWallet method
+
+					// In parallel, initialize network iframe and connect
+
+					// When keyguard returns, write transaction hash into the hidden input
+
+					// Await network and relay transaction
+
+					// Await "transaction-relayed" event from network and submit form, for real this time
+
+					// Let the user handle potential validation errors
+					// (DEBUG: check that the transaction_hash is still filled out on validation error)
+				</script>
+			<?php
+		}
+
+		public function validate_fields() {
+			return true;
+		}
+
+		/**
+		 * Output for the order received page.
+		 */
+		public function thankyou_page() {
+			if ( $this->instructions ) {
+				echo wpautop( wptexturize( $this->instructions ) );
+			}
+		}
+
+
+		/**
+		 * Add content to the WC emails.
+		 *
+		 * @access public
+		 * @param WC_Order $order
+		 * @param bool $sent_to_admin
+		 * @param bool $plain_text
+		 */
+		public function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
+
+			if ( $this->instructions && ! $sent_to_admin && $this->id === $order->get_payment_method() && $order->has_status( 'on-hold' ) ) {
+				echo wpautop( wptexturize( $this->instructions ) ) . PHP_EOL;
+			}
+		}
+
+
+		/**
+		 * Process the payment and return the result
+		 *
+		 * @param int $order_id
+		 * @return array
+		 */
+		public function process_payment( $order_id ) {
+
+			$order = wc_get_order( $order_id );
+
+			// Mark as on-hold (we're awaiting the payment)
+			$order->update_status( 'on-hold', __( 'Awaiting Nimiq payment', 'wc-gateway-nimiq' ) );
+
+			// Reduce stock levels
+			wc_reduce_stock_levels($order_id);
+
+			// Remove cart
+			WC()->cart->empty_cart();
+
+			// Return thankyou redirect
+			return array(
+				'result' 	=> 'success',
+				'redirect'	=> $this->get_return_url( $order )
+			);
+		}
+
+		// Check if the store NIM address is set
+		// Custom function not required by the Gateway
+		public function do_store_nim_address_check() {
+			if( $this->enabled == "yes" ) {
+				if( $this->get_option( 'nimiq_address' ) == "" ) {
+					echo "<div class=\"error\"><p>". sprintf( __( "You must fill in your store's Nimiq address to be able to take payments in NIM. <a href=\"%s\">Set your Nimiq address here.</a>" ), admin_url( 'admin.php?page=wc-settings&tab=checkout&section=nimiq_gateway' ) ) ."</p></div>";
+				}
+			}
+		}
+
+	} // end \WC_Gateway_Nimiq class
+}
