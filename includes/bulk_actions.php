3cdc77b7e17e75cd257cc0c21730106aa3815553
sisou <hello@soerenschwert.de>
2019-01-08 10:06:15 +0100

    Clean up comments and commented code in bulk_actions

--- includes/bulk_actions.php
+++ includes/bulk_actions.php
@@ -65,29 +65,23 @@ function _do_bulk_validate_transactions( $gateway, $ids ) {
 		$errors[] = 'Could not get the current blockchain height from the API.';
 		return [ 'changed' => $count_orders_updated, 'errors' => $errors ];
 	}
-	// echo "Current height: " . $current_height . "\n";
 
 	foreach ( $ids as $postID ) {
 		if ( !is_numeric( $postID ) ) {
 			continue;
 		}
 
-		// echo "Post ID: " . $postID . "\n";
-
 		$order = new WC_Order( (int) $postID );
 
-		// echo "Post status: " . $order->get_status() . "\n";
 		// Only continue if order status is currently 'on hold'
 		if ( $order->get_status() !== 'on-hold' ) continue;
 
 		// Convert HEX tx hash into base64
 		$transaction_hash = $order->get_meta('transaction_hash');
 		$transaction_hash = urlencode( base64_encode( pack( 'H*', $transaction_hash ) ) );
-		// echo "Hash conversion: " . $order->get_meta('transaction_hash') . ' => ' . $transaction_hash . "\n";
 
 		// Retrieve tx data from API
 		$url = $gateway->api_domain . '/transaction/' . $transaction_hash;
-		// echo "API URL: " . $url . "\n";
 		$transaction = wp_remote_get( $url );
 		if ( is_wp_error( $transaction ) ) {
 			$errors[] = $transaction->errors[ 0 ];
@@ -95,20 +89,13 @@ function _do_bulk_validate_transactions( $gateway, $ids ) {
 		}
 		$transaction = json_decode( $transaction[ 'body' ] );
 
-		// var_dump($transaction);
-
-		// FIXME: Obsolete when API returns mempool transactions
+		// TODO: Obsolete when API returns mempool transactions
 		if ( $transaction->error === 'Transaction not found' ) {
-			// echo "ERROR: Transaction not found\n";
 			// Check if order date is earlier than setting(tx_wait_duration) ago
 			$order_date = $order->get_data()[ 'date_created' ]->getTimestamp();
-			// echo "Order date: " . $order_date . "\n";
-
 			$time_limit = strtotime( '-' . $gateway->get_option( 'tx_wait_duration' ) . ' minutes' );
-			// echo "Time limit: " . $time_limit . "\n";
 			if ( $order_date < $time_limit ) {
 				// If order date is earlier, mark as failed
-				// echo "Tx not found => order failed\n";
 				fail_order( $order, 'Transaction not found within wait duration.', true );
 				$count_orders_updated++;
 			}
@@ -124,71 +111,54 @@ function _do_bulk_validate_transactions( $gateway, $ids ) {
 			continue;
 		}
 
-		// If tx is returned, validate it
+		// If a tx is returned, validate it
 
-		// echo "Transaction sender: " . $transaction->sender_address . "\n";
-		// echo "Order sender:       " . $order->get_meta('customer_nim_address') . "\n";
 		if ( $transaction->sender_address !== $order->get_meta('customer_nim_address') ) {
-			// echo "Transaction sender not equal order customer NIM address\n";
 			fail_order( $order, 'Transaction sender does not match.', true );
 			$count_orders_updated++;
 			continue;
 		}
-		// echo "OK Transaction sender matches\n";
 
-		// echo "Transaction recipient: " . $transaction->receiver_address . "\n";
-		// echo "Store address:         " . $gateway->get_option( 'nimiq_address' ) . "\n";
 		if ( $transaction->receiver_address !== $gateway->get_option( 'nimiq_address' ) ) {
-			// echo "Transaction recipient not equal store NIM address\n";
 			fail_order( $order, 'Transaction recipient does not match.', true );
 			$count_orders_updated++;
 			continue;
 		}
-		// echo "OK Transaction recipient matches\n";
 
-		// echo "Transaction value: " . $transaction->value . "\n";
-		// echo "Order value:       " . intval( $order->get_data()[ 'total' ] * 1e5 ) . "\n";
 		if ( $transaction->value !== intval( $order->get_data()[ 'total' ] * 1e5 ) ) {
-			// echo "Transaction value and order value are not equal\n";
 			fail_order( $order, 'Transaction value does not match.', true );
 			$count_orders_updated++;
 			continue;
 		}
-		// echo "OK Transaction value matches\n";
 
-		// echo "Transaction data: " . $transaction->data . "\n";
+		// Validate transaction data to include correct shortened order hash
 		$extraData = base64_decode( $transaction->data );
 		$message = mb_convert_encoding( $extraData, 'UTF-8' );
-		// echo "Transaction message: " . $message . "\n";
+		// Look for the last pair of round brackets in the tx message
 		preg_match_all( '/.*\((.*?)\)/', $message, $matches, PREG_SET_ORDER );
 		$tx_order_hash = end( $matches )[1];
-		// echo "Transaction order hash: " . $tx_order_hash . "\n";
 		$order_hash = $order->get_meta('order_hash');
 		$order_hash = strtoupper( $gateway->get_short_order_hash( $order_hash ) );
-		// echo "Order hash: " . $order_hash . "\n";
 		if ( $tx_order_hash !== $order_hash ) {
-			// echo "Transaction order hash and order hash are not equal\n";
 			fail_order( $order, 'Transaction order hash does not match.', true );
 			$count_orders_updated++;
 			continue;
 		}
-		// echo "OK Transaction order hash matches\n";
 
-		// Mark as 'processing' if confirmed
-		// echo "Transaction height: " . $transaction->block_height . "\n";
-		// echo "Confirmations setting: " . $gateway->get_option( 'confirmations' ) . "\n";
-		// echo "Transaction confirmations: " . ($current_height - $transaction->block_height) . "\n";
+		// Check if transaction is 'confirmed' yet according to confirmation setting
 		if ( empty( $transaction->block_height ) || $current_height - $transaction->block_height < $gateway->get_option( 'confirmations' ) ) {
-			// echo "Transaction valid but not yet confirmed\n";
+			// Transaction valid but not yet confirmed
 			continue;
 		}
-		// echo "OK Transaction confirmed\n";
 
+		// Mark as 'processing' when confirmed
 		$order->update_status( 'processing', 'Transaction validated and confirmed.', true );
 		$count_orders_updated++;
-	} // end for loop
+
+	} // end foreach loop
 
 	return [ 'changed' => $count_orders_updated, 'errors' => $errors ];
+
 } // end _do_bulk_validate_transactions()
 
 function fail_order($order, $reason, $is_manual_change) {
