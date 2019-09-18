7fb66e5400b2cd0d00d5c4aa456c2ec0ff487469
sisou <hello@soerenschwert.de>
2019-09-18 17:07:44 +0200

    Enable error output in webhook

--- includes/webhook.php
+++ includes/webhook.php
@@ -1,8 +1,8 @@
 <?php
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR);
 
 add_action( 'woocommerce_api_nimiq_checkout_callback', 'woo_nimiq_checkout_callback' );
 
