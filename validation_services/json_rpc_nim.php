cecb0e78a1c9d0d173c89ec123294ab17c2042c7
Sören <hello@soerenschwert.de>
2022-01-10 22:49:23 +0100

    Add dynamic transaction detection support for Nimiq JSON-RPC API

--- validation_services/json_rpc_nim.php
+++ validation_services/json_rpc_nim.php
@@ -2,6 +2,9 @@
 include_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'interface.php' );
 
 class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validation_Service_Interface {
+    // Constants
+    const API_TX_PER_PAGE = 25;
+
     /**
      * Initializes the validation service
      * @param {WC_Gateway_Nimiq} $gateway - A WC_Gateway_Nimiq class instance
@@ -9,7 +12,13 @@ class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validati
      */
     public function __construct( $gateway ) {
         $this->transaction = null;
+        $this->payment_state = null;
+        $this->transactions = [];
         $this->head_height = null;
+        $this->limit = 0;
+
+        $this->username = $gateway->get_option( 'jsonrpc_nimiq_username' );
+        $this->password = $gateway->get_option( 'jsonrpc_nimiq_password' );
 
         $this->api_domain = $gateway->get_option( 'jsonrpc_nimiq_url' );
         if ( empty( $this->api_domain ) ) {
@@ -26,9 +35,7 @@ class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validati
             return $this->head_height;
         }
 
-        $call = '{"jsonrpc":"2.0","method":"blockNumber","params":[],"id":42}';
-
-        $api_response = wp_remote_post( $this->api_domain, array( 'body' => $call ) );
+        $api_response = wp_remote_post( $this->api_domain, $this->make_request( "blockNumber" ) );
 
         if ( is_wp_error( $api_response ) ) {
             return $api_response;
@@ -56,53 +63,114 @@ class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validati
      * @return {'NOT_FOUND'|'PAID'|'OVERPAID'|'UNDERPAID'|WP_Error}
      */
     public function load_transaction( $transaction_hash, $order, $gateway ) {
+        // Reset loaded transaction
         $this->transaction = null;
+        $this->payment_state = null;
 
-        // Automatic transaction finding is not yet available for Nimiq
-        if ( empty( $transaction_hash ) ) return 'NOT_FOUND';
+        $recipient_address = null;
 
-        if ( !ctype_xdigit( $transaction_hash ) ) {
-            return new WP_Error( 'connection', __( 'Invalid transaction hash.', 'wc-gateway-nimiq' ) );
+        if ( !empty( $transaction_hash ) ) {
+            if ( !ctype_xdigit( $transaction_hash ) ) {
+                return new WP_Error('service', __( 'Invalid transaction hash.', 'wc-gateway-nimiq' ) );
+            }
+
+            $api_response = wp_remote_post( $this->api_domain, $this->make_request( "getTransactionByHash", [ $transaction_hash ] ) );
+
+            if ( is_wp_error( $api_response ) ) {
+                return $api_response;
+            }
+
+            $response = json_decode( $api_response[ 'body' ] );
+
+            if ( $response->error ) {
+                return new WP_Error( 'service', 'JSON-RPC replied: ' . $response->error->message );
+            }
+
+            if ( empty( $response ) ) {
+                return new WP_Error( 'service', sprintf( __( 'Could not retrieve transaction information from %s.', 'wc-gateway-nimiq' ), 'JSON-RPC server') . ' (' . $api_response[ 'response' ][ 'code' ] . ': ' . $api_response[ 'response' ][ 'message' ] . ')' );
+            }
+
+            $this->transaction = $response->result;
+            return $this->transaction_found() ? 'PAID' : 'NOT_FOUND';
         }
+
+        $recipient_address = Order_Utils::get_order_recipient_address( $order, $gateway );
+        // TODO: NIM Address validation
+        // if ( !AddressValidator::isValid( $recipient_address ) ) {
+        //     return new WP_Error('service', __( 'Invalid merchant address.', 'wc-gateway-nimiq' ) );
+        // }
 
         $head_height = $this->blockchain_height();
         if ( is_wp_error( $head_height ) ) {
             return $head_height;
         }
 
-        $call = '{"jsonrpc":"2.0","method":"getTransactionByHash","params":["' . $transaction_hash . '"],"id":42}';
+        // Use cached results, if any
+        $response = $this->transactions;
+        $limit = $this->limit;
 
-        $username = $gateway->get_option( 'jsonrpc_nimiq_username' );
-        $password = $gateway->get_option( 'jsonrpc_nimiq_password' );
-        $headers = array( );
+        while ( !$this->transaction ) {
+            $transaction = $this->find_transaction( $recipient_address, $order, $response, $gateway );
+            if ( $transaction && $this->payment_state ) {
+                $this->transaction = $transaction;
+                // Store tx hash in order
+                $order->update_meta_data( 'transaction_hash', $transaction->hash );
+                $order->update_meta_data( 'customer_nim_address', $transaction->fromAddress );
+                $order->update_meta_data( 'nc_payment_state', $this->payment_state );
+                $order->save();
+                return $this->payment_state;
+            }
 
-        if ( !empty( $username ) || !empty( $password ) ) {
-            $authorization = 'Basic ' . base64_encode( $username . ":" . $password );
-            $headers['Authorization'] = $authorization;
+            if ( $this->payment_state === 'UNDERPAID' ) return $this->payment_state;
+
+            // Stop when no more transactions are available
+            // ($limit is set to -1 below, when a paged API call returns less than $limit transactions)
+            if ( $limit < 0 ) {
+                return 'NOT_FOUND';
+            }
+
+            // Stop when earliest transaction is earlier than the order date
+            $order_date = $order->get_data()[ 'date_created' ]->getTimestamp();
+            if ( end( $response ) && ( empty( end( $response )->timestamp ) || end( $response )->timestamp < $order_date ) ) {
+                return 'NOT_FOUND';
+            }
+
+            $limit += self::API_TX_PER_PAGE;
+            $api_response = wp_remote_post( $this->api_domain, $this->get_request_transactions_by_address( $recipient_address, $limit ) );
+
+            if ( is_wp_error( $api_response ) ) {
+                return $api_response;
+            }
+
+            $response = json_decode( $api_response[ 'body' ] );
+
+            if ( $response->error ) {
+                return new WP_Error( 'service', 'JSON-RPC replied: ' . $response->error->message );
+            }
+
+            if ( empty( $response ) ) {
+                return new WP_Error( 'service', sprintf( __( 'Could not retrieve account transactions from %s.', 'wc-gateway-nimiq' ), 'JSON-RPC server') . ' (' . $api_response[ 'response' ][ 'code' ] . ': ' . $api_response[ 'response' ][ 'message' ] . ')' );
+            }
+
+            $response = $response->result;
+
+            function cmp($a, $b) {
+                if ($a->timestamp === $b->timestamp) return 0;
+                if (!$a->timestamp) return -1;
+                if (!$b->timestamp) return 1;
+                return ($a->timestamp > $b->timestamp) ? -1 : 1;
+            }
+            usort( $response, "cmp");
+
+            // Check number of returned transactions
+            if ( count( $response ) < $limit ) {
+                $limit = -1; // Signal that all transactions have been fetched
+            }
+
+            // Cache API results
+            $this->add_transactions( $response );
+            $this->limit = $limit;
         }
-
-        $api_response = wp_remote_post($this->api_domain, array(
-                'headers' => $headers,
-                'body' => $call,
-            )
-        );
-
-        if ( is_wp_error( $api_response ) ) {
-            return $api_response;
-        }
-
-        $response = json_decode( $api_response[ 'body' ] );
-
-        if ( $response->error ) {
-            return new WP_Error( 'service', 'JSON-RPC replied: ' . $response->error->message );
-        }
-
-        if ( empty( $response ) ) {
-            return new WP_Error( 'service', sprintf( __( 'Could not retrieve transaction information from %s.', 'wc-gateway-nimiq' ), 'JSON-RPC server') . ' (' . $api_response[ 'response' ][ 'code' ] . ': ' . $api_response[ 'response' ][ 'message' ] . ')' );
-        }
-
-        $this->transaction = $response->result;
-        return $this->transaction_found() ? 'PAID' : 'NOT_FOUND';
     }
 
     /**
@@ -150,11 +218,7 @@ class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validati
      * @return {string}
      */
     public function message() {
-        if ( $this->transaction->data === null ) {
-            return '';
-        }
-
-        $extraData = hex2bin( $this->transaction->data );
+        $extraData = hex2bin( $this->transaction->data ?: '' );
         return mb_convert_encoding( $extraData, 'UTF-8' );
     }
 
@@ -171,8 +235,68 @@ class WC_Gateway_Nimiq_Service_JsonRpcNimiq implements WC_Gateway_Nimiq_Validati
      * @return {number}
      */
     public function confirmations() {
-        if ( empty( $this->block_height() ) ) return 0;
-        return $this->blockchain_height() + 1 - $this->block_height();
+        return $this->transaction->confirmations;
+    }
+
+    private function find_transaction( $recipient_address, $order, $transactions, $gateway ) {
+        $order_date = $order->get_data()[ 'date_created' ]->getTimestamp();
+        foreach ( $transactions as $tx ) {
+            // Check that tx is not too old
+            if (!empty( $tx->timestamp ) && $tx->timestamp < $order_date) continue;
+            if ( $tx->toAddress === $recipient_address ) {
+                // If tx has a message, check that it matches
+                $extraData = hex2bin( $tx->data ?: '' );
+                $message = mb_convert_encoding( $extraData, 'UTF-8' );
+                if ( !empty( $message ) ) {
+                    // Look for the last pair of round brackets in the tx message
+                    preg_match_all( '/.*\((.*?)\)/', $message, $matches, PREG_SET_ORDER );
+                    $tx_order_key = end( $matches )[1];
+                    if ( $tx_order_key !== $gateway->get_short_order_key( $order->get_order_key() ) ) {
+                        continue;
+                    }
+                }
+
+                $comparison = Crypto_Manager::unit_compare( $tx->value, Order_Utils::get_order_total_crypto( $order ) );
+                $this->payment_state = Order_Utils::get_payment_state( $comparison );
+                if ( $comparison >= 0 ) return $tx;
+            }
+        }
+        return null;
+    }
+
+    private function add_transactions( $transactions ) {
+        foreach ( $transactions as $tx ) {
+            $this->transactions[ $tx->hash ] = $tx;
+        }
+    }
+
+    private function make_request( $method, $params = [] ) {
+        $headers = array(
+            // Need to set referer to emtpy string, as Wordpress sets it to the site domain,
+            // but the Nimiq JsonRpcServer rejects the request (403) when a referer is set.
+            'Referer' => '',
+        );
+
+        if ( !empty( $this->username ) || !empty( $this->password ) ) {
+            $authorization = 'Basic ' . base64_encode( $this->username . ":" . $this->password );
+            $headers['Authorization'] = $authorization;
+        }
+
+        $call = json_encode([
+            'jsonrpc' => "2.0",
+            'method' => $method,
+            'params' => $params,
+            'id' => 42,
+        ]);
+
+        return array(
+            'headers' => $headers,
+            'body' => $call,
+        );
+    }
+
+    private function get_request_transactions_by_address( string $address, int $limit ) {
+        return $this->make_request( "getTransactionsByAddress", [ $address, $limit ] );
     }
 }
 
